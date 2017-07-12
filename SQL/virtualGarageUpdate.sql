18ae12866aa32dfb4c447a0a10316b7c56447f82
oiad <oiad@skeleton.net.nz>
2017-07-12 21:40:08 +1200

    Revert charchange, it crashes extdb

--- SQL/virtualGarageUpdate.sql
+++ SQL/virtualGarageUpdate.sql
@@ -2,9 +2,6 @@
 	Only use this sql file if you are updating from a previous version.
 	Use SQL\virtualGarage.sql if you are starting fresh.
 */
-
-ALTER TABLE `garage` CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE `garage` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE `garage` ADD `Name` VARCHAR(50) NOT NULL DEFAULT '' AFTER `PlayerUID`;
 ALTER TABLE `garage` ADD `displayName` VARCHAR(50) NOT NULL DEFAULT '' AFTER `Name`;
 
